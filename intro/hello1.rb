556f268c52111ecc8a3a1594cdc49db75c26c153
Tung Dong Viet <tungdongviet3@gmail.com>
2025-09-14 03:12:09 +0700

    Demo Expression Interpolation

---
+++ intro/hello1.rb
@@ -0,0 +1,9 @@
+def say_hello_goodbye(name)
+  # result = "I don't know why you say goodbye, " + name + ", I say hello"
+  # return result
+  "I don't know why you say goodbye, #{name}, I say hello" 
+end
+
+# call the method
+puts say_hello_goodbye("John")
+puts say_hello_goodbye("Paul")
