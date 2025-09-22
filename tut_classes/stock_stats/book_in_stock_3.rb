a693e56dc8cdd475e679984aceab1576f46cdf74
Tung Dong Viet <tungdongviet3@gmail.com>
2025-09-22 22:31:04 +0700

    Demo Classes, Attributes, Methods

---
+++ tut_classes/stock_stats/book_in_stock_3.rb
@@ -0,0 +1,18 @@
+class BookInStock
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = price
+  end
+
+  def isbn
+    @isbn
+  end 
+
+  def price
+    @price
+  end
+end
+
+book = BookInStock.new("isbn1", 12.34)
+puts "ISBN = #{book.isbn}"
+puts "Price = #{book.price}"
