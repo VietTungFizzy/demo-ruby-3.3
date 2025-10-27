9477570313b2cb66f034d54ee565a30555a4b2c4
Tung <31438811+VietTungFizzy@users.noreply.github.com>
2025-10-27 11:21:39 +0700

    Add state management to Client class
    Implement state management for Client instances using a module.

---
+++ tut_modules/state_eg.rb
@@ -0,0 +1,24 @@
+module Test
+  def self.states
+    @states ||= {}
+  end
+
+  def state=(value)
+    Test.states[object_id] = value
+  end
+
+  def state
+    Test.states[object_id]
+  end
+end
+
+class Client
+  include Test
+end
+
+c1 = Client.new
+c2 = Client.new
+c1.state = "cat"
+c2.state = "dog"
+puts c1.state
+puts c2.state
