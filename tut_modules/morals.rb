485c2b7b4be4a113fe0f1dab709df3dc4fb2f44c
VietTungFizzy <minhmang67@yahoo.com.vn>
2025-10-27 10:12:58 +0700

    Demo include feature

---
+++ tut_modules/morals.rb
@@ -0,0 +1,7 @@
+module Morals
+  VERY_BAD=0
+  BAD = 1
+  def self.sin(badness)
+
+  end
+end
---
+++ tut_modules/pin_head.rb
@@ -0,0 +1,5 @@
+require_relative "trig"
+require_relative "morals"
+
+y = Trig.sin(Trig::PI / 4)
+wrongdoing = Morals.sin(Morals::VERY_BAD)
